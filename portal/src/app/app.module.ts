8c7a819f71d1ee284f22b9893ee2595d75b8dee6
Lucas Norberto <lucasnorberto@gmail.com>
2021-09-01 23:01:16 -0300

    menu

--- portal/src/app/app.module.ts
+++ portal/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './controle/home/home.component';
+import { NavbarComponent } from './nav/navbar/navbar.component';
+import { FooterComponent } from './nav/footer/footer.component';
+import { ContatoComponent } from './controle/contato/contato.component';
+import { AjudaComponent } from './controle/ajuda/ajuda.component';
+import { SobreComponent } from './controle/sobre/sobre.component';
+import { ServicoComponent } from './controle/servico/servico.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContatoComponent,
+    AjudaComponent,
+    SobreComponent,
+    ServicoComponent
   ],
   imports: [
     BrowserModule,
